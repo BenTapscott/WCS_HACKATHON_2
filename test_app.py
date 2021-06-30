52bd3a05c37217ad96f99892d1229a2bf5f3a879
Ben Tapscott <bentapscottpro@gmail.com>
2021-06-30 12:07:49 +0200

    16

--- test_app.py
+++ test_app.py
@@ -14,11 +14,11 @@ df_nlp = pd.read_csv(link)
 seller_level = st.sidebar.selectbox('Choose seller level',
     ('Top Rated Seller', 'Level 1 Seller','Level 2 Seller'))
 
+search_term = st.sidebar.text_input('Search for key words', 'website design')
+
 st.write('Current search term is :', search_term)
 
 st.subheader('Data with filters applied : ')
-
-search_term = st.sidebar.text_input('Search for key words', 'website design')
 
 df_nlp.loc[(df_nlp['seller_level']==seller_level) & (df_nlp['tf_idf'].str.contains(search_term))]
 
