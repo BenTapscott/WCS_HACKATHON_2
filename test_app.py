24880dc9a9412c1b0748f973952836c2e1b761ef
Ben Tapscott <bentapscottpro@gmail.com>
2021-06-30 12:04:13 +0200

    15

--- test_app.py
+++ test_app.py
@@ -3,10 +3,7 @@ import streamlit as st
 import pandas as pd
 
 
-st.title('Welcome to our app')
-
-
-st.write("Select the gig")
+st.title('Welcome to the Fiverr Seller Advisor')
 
 st.sidebar.header('User Input Parameters')
 
@@ -17,11 +14,11 @@ df_nlp = pd.read_csv(link)
 seller_level = st.sidebar.selectbox('Choose seller level',
     ('Top Rated Seller', 'Level 1 Seller','Level 2 Seller'))
 
-st.subheader('Show DF filtered')
+st.write('Current search term is :', search_term)
+
+st.subheader('Data with filters applied : ')
 
 search_term = st.sidebar.text_input('Search for key words', 'website design')
-
-st.write('Current search term is :', search_term)
 
 df_nlp.loc[(df_nlp['seller_level']==seller_level) & (df_nlp['tf_idf'].str.contains(search_term))]
 
