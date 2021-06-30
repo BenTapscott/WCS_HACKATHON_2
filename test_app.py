e6f5a13f36b7cd02aafe6f460ac5f22d4f3e8a99
Ben Tapscott <bentapscottpro@gmail.com>
2021-06-30 12:28:31 +0200

    viz4

--- test_app.py
+++ test_app.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
+import seaborn as sns
+
 
 st.title('Welcome to the Fiverr Seller Advisor')
 
@@ -24,11 +26,14 @@ st.subheader('Data with filters applied : ')
 
 df_nlp.loc[(df_nlp['seller_level']==seller_level) & (df_nlp['tf_idf'].str.contains(search_term))]
 
+plt.style.use('dark_background')
+
 fig, ax = plt.subplots(figsize=(18,8))
-ax.hist(df_nlp['len_description_content'].loc[(df_nlp['seller_level']==seller_level) & (df_nlp['tf_idf'].str.contains(search_term))],  density=True);
+ax.hist(df_nlp['len_description_content'].loc[(df_nlp['seller_level']==seller_level) & (df_nlp['tf_idf'].str.contains(search_term))]);
 ax.plot( linewidth=1);
-ax.set_title('len description');
-ax.set_xlabel('Number of words');
+ax.set_title('length of gig description');
+ax.set_xlabel('Number of words')
+ax.set_ylabel('Number of gigs');
 st.pyplot(fig)
 
 #df_nlp
